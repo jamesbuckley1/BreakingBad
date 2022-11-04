bedac4270ec8bc334b6681ec15da40d677d7860f
James Buckley <james.buckley@polestar-eam.com>
2022-11-04 23:55:14 +0000

    Initial Commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by James Buckley on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
